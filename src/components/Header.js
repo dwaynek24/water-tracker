112cdcbab47bf27d45958070ad1732923a118046
dwaynek24 <devinwayneknotts@gmail.com>
2022-11-09 09:04:57 -0800

    configure Mui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 function Header(props) {
+    
     return (
         <div>
             <h1>Water Tracker</h1>
-            
+         
         </div>
     );
 }
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
+
+const marks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 25,
+    label: '25',
+  },
+  {
+    value: 50,
+    label: '50 ',
+  },
+  {
+    value: 75,
+    label: '75 ',
+  },
+  {
+    value: 100,
+    label: '100',
+  },
+];
+
+function valuetext(value) {
+  return `${value}°C`;
+}
 function InputForm(props) {
     return (
-        <div>
-            
+        <div className = 'inputForm'>
+            <h2 className = 'inputForm' > Minutes</h2>
+            <Box sx={{ width: 300 }}>
+      <Slider
+        aria-label="Always visible"
+        defaultValue={0}
+        getAriaValueText={valuetext}
+        step={10}
+        marks={marks}
+        valueLabelDisplay="on"
+      />
+    </Box>
         </div>
     );
 }
