fa2912bef88f40ad54b4c2a149317a660b6ba14a
dwaynek24 <devinwayneknotts@gmail.com>
2022-11-10 11:42:20 -0800

    user component

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
 
+const markss = [
+    {
+        value: 0,
+        label: '0',
+      },
+      {
+        value: 1,
+        label: '1',
+      },
+      {
+        value: 2,
+        label: '2',
+      },
+      {
+        value: 3,
+        label: '3 ',
+      },
+      {
+        value: 4,
+        label: '4',
+      },
+
+];
 
 const marks = [
   {
@@ -27,23 +55,64 @@ const marks = [
 ];
 
 function valuetext(value) {
-  return `${value}°C`;
+  return `${value}`;
 }
+function valuetext2(value2) {
+    return `${value2}`;
+  }
 function InputForm(props) {
+    const [type, setType] =useState('');
+
+    const handleChange = (event) => {
+      setType(event.target.value);
+    };
     return (
-        <div className = 'inputForm'>
-            <h2 className = 'inputForm' > Minutes</h2>
+        <main className = 'inputForm'>
+            <h2 className = 'inputForm'> Activity</h2>
+            <div>
+            <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Type</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={type}
+          label="Type"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Shower</MenuItem>
+          <MenuItem value={1}>Bath</MenuItem>
+          <MenuItem value={1}>At Home Car Wash</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+            </div >
+            <div>
+            <h2>Times per day</h2>
+            <Box sx={{ width: 300 }}>
+      <Slider
+        aria-label="Always visible"
+        defaultValue={0}
+        getAriaValueText={valuetext2}
+        step={1}
+        markss={markss}
+        valueLabelDisplay="on"
+      />
+    </Box>
+    </div >
+    
+            <h2 className = 'inputForm'> Minutes</h2>
             <Box sx={{ width: 300 }}>
       <Slider
         aria-label="Always visible"
         defaultValue={0}
         getAriaValueText={valuetext}
-        step={10}
+        step={1}
         marks={marks}
         valueLabelDisplay="on"
       />
     </Box>
-        </div>
+        </main>
     );
 }
 
