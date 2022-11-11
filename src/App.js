eb795a111cd1baa6652a54efe19dd657ad640dbf
dwaynek24 <devinwayneknotts@gmail.com>
2022-11-11 12:09:31 -0800

    update chart

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import InputForm from './components/InputForm'
 import Chart from './components/Chart';
+import Chart2 from './components/Chart2';
 
 function App() {
   return (
     <div className="App">
      <Header />
      <InputForm />
-     <Chart />
+     
+     <Chart2 />
     </div>
   );
 }
