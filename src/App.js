bd79bb42795ceecdd69057b6a1ab27038afdbbbb
dwaynek24 <devinwayneknotts@gmail.com>
2022-11-10 19:08:56 -0800

    add chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header';
 import InputForm from './components/InputForm'
+import Chart from './components/Chart';
 
 function App() {
   return (
     <div className="App">
      <Header />
      <InputForm />
+     <Chart />
     </div>
   );
 }
